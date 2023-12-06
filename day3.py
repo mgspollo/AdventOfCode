c233ef4ababd51041fe9f90af36b708ee5f20a61
MarkPollock <mark.gs.pollock@gmail.com>
2023-12-06 14:41:49 +0000

    initial commit

---
+++ day3.py
@@ -0,0 +1,65 @@
+import numpy as np
+import pandas as pd
+import string
+
+with open('input/day3.txt') as f:
+    lines = f.readlines()
+    input_lines = []
+    for line in lines:
+        input_lines.append([str(i) for i in line.strip('\n')])
+
+def check_adjacent_cells(row, start_col, end_col):
+    symbol_list = ['!', '£', '$', '%', '^', '&', '*', '@', '#', '+', '/', '-', '=']
+    for i_a in range(row - 1, row + 2):
+        for j_a in range(start_col - 1, end_col + 1):
+            cell = a_pad[i_a, j_a]
+            if any(cell == x for x in symbol_list):
+                return True
+    return False
+
+def check_adjacent_cells_gears(row, start_col, end_col):
+    symbol_list = ['*']
+    for i_a in range(row - 1, row + 2):
+        for j_a in range(start_col - 1, end_col + 1):
+            cell = a_pad[i_a, j_a]
+            if any(cell == x for x in symbol_list):
+                cell_coord = (i_a, j_a)
+                return cell_coord
+    return False
+
+
+def check_gear_ratio(row, start_col, end_col):
+
+
+
+a = np.column_stack(input_lines).T
+row_num, col_num = a.shape
+a_pad = np.pad(a, 1, mode='constant', constant_values='.')
+# df_digit = pd.DataFrame(columns=['number', 'start', 'end', 'is_valid'])
+df_digit = pd.DataFrame(columns=['number', 'start', 'end', 'star_coords'])
+for i in range(1, row_num+1):
+    digit_num = 0
+    for j in range(1, col_num+1):
+        if j < digit_num:
+            j = digit_num
+        if any(a_pad[i, j] == str(x) for x in range(0, 10)):
+            digit_num = j
+            digit_str = str()
+            while any(a_pad[i, digit_num] == str(x) for x in range(0, 10)):
+                digit_str += a_pad[i, digit_num]
+                digit_num += 1
+            df_digit.loc[len(df_digit.index)] = [
+                int(digit_str),
+                (i, j),
+                (i, digit_num),
+                check_adjacent_cells_gears(i, j, digit_num)
+            ]
+            j = digit_num
+            # df_digit['number'] = int(digit_str)
+            # df_digit['start'] = (i, j)
+            # df_digit['end'] = (i, digit_num)
+            # df_digit['is_valid'] = check_adjacent_cells(i, j, digit_num)
+
+df_digit_gears = df_digit[df_digit.star_coords]
+
+
---
+++ day4.py
@@ -0,0 +1,37 @@
+with open('input/day4.txt') as f:
+    input = f.readlines()
+
+def get_card_values(line_input, win_list, num_copies, line_num):
+    card_num, card_input = line_input.strip('\n').split(':', maxsplit=2)
+    if line_num < 10:
+        card_num = int(card_num.split('   ', maxsplit=2)[1])
+    elif line_num < 100:
+        card_num = int(card_num.split('  ', maxsplit=2)[1])
+    else:
+        card_num = int(card_num.split(' ', maxsplit=2)[1])
+    win_num, scr_num = card_input.split('|', maxsplit=2)
+
+    wins_set = set([int(i.strip()) for i in win_num.strip().split(' ') if i != ''])
+    scrs_set = set([int(i.strip()) for i in scr_num.strip().split(' ') if i != ''])
+
+    inter = set.intersection(wins_set, scrs_set)
+    exp = len(inter) - 1
+
+    if num_copies > 0:
+        if card_num+exp >= len(input)+1:
+            upper_lim = len(input)+1
+        else:
+            upper_lim = card_num+exp+1
+        win_list += [i for i in range(card_num+1, upper_lim+1) for _ in range(num_copies)]
+
+    return win_list
+    # win_nums = [i.strip() for i in win_num.strip().split(' ')]
+    # scr_nums = [i.strip() for i in scr_num.strip().split(' ')]
+
+
+cum_game_score = 0
+win_list = list(range(1, len(input)+1))
+for line, w in zip(input, win_list):
+    win_list = get_card_values(line, win_list, num_copies=win_list.count(w), line_num=w)
+
+print(len(win_list))
